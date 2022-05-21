33261a0453b2e7743d7182a110aaa237ff725c27
BuildTools <unconfigured@null.spigotmc.org>
2022-05-21 16:07:18 +0200

    add class for player

--- index.js
+++ index.js
@@ -7,49 +7,71 @@ app.use(express.static(__dirname + '/views'));
 var player_max_id = 0
 var playerlist = {}
 
-app.ws('/ws', (ws, req) => {    
-    var player_id = player_max_id++
+class Player {
+    constructor(ws) {
+        this.ws = ws
+        this.id = player_max_id++
+    }
 
+    sendMsg( obj ) {
+        var msg = JSON.stringify(obj)
+        this.ws.send(msg)
+    }
+
+    onReceiveMsg( callback ) {
+        this.ws.on('message', (msg)=>{
+            var obj = JSON.parse(msg)
+            callback(obj)
+        })
+    }
+
+    onDisconnect( callback ) {
+        this.ws.on('close', callback)
+    }
+}
+
+app.ws('/ws', (ws, req) => {    
     var player = new Player(ws)
 
-    console.log("join player "+player_id)
+    console.log("join player "+player.id)
 
     // send to self
-    ws.send(JSON.stringify({
+    player.sendMsg({
         action: "join",
-        id: player_id,
+        id: player.id,
         others: Object.keys(playerlist)
-    }))
+    })
 
     // announce to others
     for( key in playerlist ) {
-        var player_ws = playerlist[key]
-        player_ws.send(JSON.stringify({
+        var p = playerlist[key]
+        p.sendMsg({
             action: "announce",
-            id: player_id
-        }))
+            id: player.id
+        })
     }
 
     // add player to list
-    playerlist[player_id] = ws
-    
-    ws.on('message', (msg) => {
+    playerlist[player.id] = player
+
+    // distribute incoming messages to all other players
+    player.onReceiveMsg( (msg) => {
         for( key in playerlist ) {
-            var player_ws = playerlist[key]
-            player_ws.send(msg)
+            var p = playerlist[key]
+            p.sendMsg(msg)
         }
     })
 
-    ws.on('close', (req) => {
-        console.log("leaving player "+player_id)
-        delete playerlist[player_id]
+    player.onDisconnect( (req) => {
+        console.log("leaving player "+player.id)
+        delete playerlist[player.id]
 
         for( key in playerlist ) {
-            var player_ws = playerlist[key]
-            player_ws.send(JSON.stringify({
+            var p = playerlist[key]
+            p.sendMsg({
                 action: "leave",
-                id: player_id
-            }))
+                id: player.id
+            })
         }
     })
 })
